fa900f5dd56b9e43da85afd863626b91beec0875
codervandan <dquintana.dev@gmail.com>
2026-05-27 18:28:05 -0700

    added error handling and controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,53 @@
 const User = require("../models/User");
 
 // GET /users
-
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => {
-      res.send(users);
+      res.status(200).send(users);
     })
     .catch((err) => {
       console.error(err);
+      return res
+        .status(500)
+        .send({ message: "An error occurred while fetching users" });
     });
 };
 
-module.exports = { getUsers };
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+  console.log(name, avatar);
+  User.create({ name, avatar })
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Invalid user data" });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Invalid user ID format" });
+      }
+      return res
+        .status(500)
+        .send({ message: "An error occurred while fetching the user" });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
