092d28eacb616b3b3206a1c104c36e515c3996c4
codervandan <dquintana.dev@gmail.com>
2026-05-27 20:53:58 -0700

    added hard coded id

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ const routes = require("./routes");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+
+  next();
+});
+
 app.use(routes);
 
 // 404
