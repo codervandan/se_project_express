1ef2f6270f99888ad1d8b20401e7394443e00784
codervandan <dquintana.dev@gmail.com>
2026-06-03 17:34:04 -0700

    fixed merging conflicts

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -2,10 +2,7 @@ const router = require("express").Router();
 
 const {
   createItem,
-<<<<<<< HEAD
   getItems,
-=======
->>>>>>> 755c40992ac40e52a7762e0a1dd091658f38b9a5
   deleteItem,
   likeItem,
   dislikeItem,
@@ -14,10 +11,6 @@ const {
 // CREATE
 router.post("/", createItem);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 755c40992ac40e52a7762e0a1dd091658f38b9a5
 // DELETE
 router.delete("/:itemId", deleteItem);
 
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,15 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 const userRouter = require("./users");
-<<<<<<< HEAD
 const clothingItemRouter = require("./clothingItem");
-=======
-
->>>>>>> 755c40992ac40e52a7762e0a1dd091658f38b9a5
 const { createUser, login } = require("../controllers/users");
-
-const clothingItemRouter = require("./clothingItem");
-
 
 router.post("/signup", createUser);
 router.post("/signin", login);
-
 
 router.use(auth);
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
-
-
 
 module.exports = router;
