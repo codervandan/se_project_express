12cf9f30bd9a879144c5f94a68c1f18351d4cc71
codervandan <dquintana.dev@gmail.com>
2026-06-16 18:15:48 -0700

    fix github action endpoint test

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+
+  next();
+});
+
 app.use(requestLogger);
 
 app.get("/crash-test", () => {
